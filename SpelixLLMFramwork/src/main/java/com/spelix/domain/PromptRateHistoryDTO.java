f6d918bd25d0246666474d86f6c78c7329330714
kwonsuhyun <kwonsuhyun@192.168.0.18>
2024-02-29 15:06:41 +0900

    신규 DTO 생성

---
+++ SpelixLLMFramwork/src/main/java/com/spelix/domain/PromptRateHistoryDTO.java
@@ -0,0 +1,28 @@
+package com.spelix.domain;
+
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class PromptRateHistoryDTO {
+
+	private String promptRateHistId;
+	private String promptId;
+	private String promptVer;
+	private String[] llmCustomIds;
+	private String prompt;
+	private String[] sysPromptIds;
+	private String sysPromptEtc;
+	private String parmJson;
+	private float promptRate;
+	private String insertUser;
+	private Timestamp insertDate;
+	private String updateUser;
+	private Timestamp updateDate;
+
+}
